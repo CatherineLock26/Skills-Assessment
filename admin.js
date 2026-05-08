b786da1ed09f8a69232be7e683d823effc1df737
CatherineLock26 <141253245+CatherineLock26@users.noreply.github.com>
2026-05-08 15:00:47 +0100

    Update admin.js
    Logging out admin

--- admin.js
+++ admin.js
@@ -343,3 +343,14 @@ async function logoutAdmin() {
   await sb.auth.signOut();
   location.reload();
 }
+
+document.getElementById("backLink").addEventListener("click", async (e) => {
+
+    e.preventDefault();
+
+    // LOG OUT ADMIN
+    await supabase.auth.signOut();
+
+    // RETURN TO ASSESSMENT
+    window.location.href = "index.html";
+});
